ee9d04ab98530bd8f941551a37ac82a668f1b270
Invitado_PC11 <Invitado_PC11@LAB06_PC11>
2017-10-23 09:50:07 -0500

    licha

---
+++ src/trabajo1/MoretaLisette.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo1;
+
+/**
+ *
+ * @author Invitado_PC11
+ */
+public class MoretaLisette {
+    public static void main(){
+        System.out.println("Lissette Moreta desarrollo");
+        
+
+    }
+}
