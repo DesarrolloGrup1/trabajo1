1306c00656361cb46c936c3714b840fe6a4922ef
Invitado_PC13 <Invitado_PC13@Lab06_PC13>
2017-10-23 09:40:21 -0500

    Commit Edison

---
+++ src/trabajo1/CornejoEdison.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo1;
+
+/**
+ *
+ * @author Invitado_PC13
+ */
+public class CornejoEdison {
+    public static void main(){
+        System.out.println("Edison Cornejo");
+    }
+}
